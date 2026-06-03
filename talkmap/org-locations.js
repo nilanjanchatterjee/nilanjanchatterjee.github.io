69c9dee6c6d536d9b4f4f8ec106e84ca77314035
Nilanjan Chatterjee <55828175+nilanjanchatterjee@users.noreply.github.com>
2026-06-03 18:12:50 +0200

    Update org-locations.js

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,22 +1,43 @@
 var addressPoints = [
   [
-    "Berkeley CA, USA",
+    "Bishkek, Kyrgyzstan",
     37.8708393,
     -122.2728638
   ],
   [
-    "London, UK",
+    "Birmingham, UK",
     51.5073219,
     -0.1276473
   ],
   [
-    "San Francisco, California",
+    "Kentucky, USA",
     37.7792808,
     -122.4192362
   ],
   [
-    "Los Angeles, CA",
+    "Chicago, USA",
+    34.0543942,
+    -118.2439408
+  ],
+  [
+    "Kolkata, India",
+    34.0543942,
+    -118.2439408
+  ],
+  [
+    "Lucca, Italy",
+    34.0543942,
+    -118.2439408
+  ],
+  [
+    "Nanyuki, Kenya",
+    34.0543942,
+    -118.2439408
+  ],
+  [
+    "St. Andrews, UK",
     34.0543942,
     -118.2439408
   ]
+
 ];
